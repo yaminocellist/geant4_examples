dad01a20752807936b1a74f4b4e2a1c00fdd4353
fanxin_sun <fanxin_sun@apple.com>
2025-12-19 23:50:02 +0800

    Progress 1

--- mySim/sim.cc
+++ mySim/sim.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
-#include "G4UIExecutive.hh"
 #include "G4VisExecutive.hh"
 #include "G4VisManager.hh"
+#include "G4UIExecutive.hh"
 
 #include "construction.hh"
 #include "physics.hh"
@@ -12,10 +13,7 @@
 int main(int argc, char** argv) {
     G4RunManager *runManager = new G4RunManager();
 
-    runManager -> SetUserInitialization(new MyDetectorConstruction());
-    runManager -> SetUserInitialization(new MyPhysicsList());
-    runManager -> SetUserInitialization(new MyActionInitialization());
-    runManager -> Initialize();
+    // runManager -> Initialize();
 
     G4UIExecutive *ui = new G4UIExecutive(argc, argv);
 
@@ -23,12 +21,6 @@ int main(int argc, char** argv) {
     visManager -> Initialize();
 
     G4UImanager *UImanager = G4UImanager::GetUIpointer();
-    UImanager -> ApplyCommand("/vis/open OGL 900x900-100+0");
-    UImanager -> ApplyCommand("/vis/drawVolume");
-    UImanager -> ApplyCommand("/vis/viewer/set/viewpointVector 1 0.8 0.7");
-    UImanager -> ApplyCommand("/vis/viewer/autoRefresh true");
-    UImanager -> ApplyCommand("/vis/scene/add/trajectories smooth");
-    UImanager -> ApplyCommand("/vis/scene/endOfEventAction accumulate");
 
     ui -> SessionStart();
 
